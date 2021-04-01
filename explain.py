76b37c0c6b2f38ace61d421da6e7406263edc5cf
Nelsonchris1 <ogbeide331@gmail.com>
2021-04-01 17:57:40 +0100

    Function (shap values) Values

--- explain.py
+++ explain.py
@@ -38,7 +38,20 @@ def perm_import_plot(plot_importance):
 #Partial dependeny plot
 
 def pdplot(model, X_val, feat):
+    ml_model = pickle.load(open(model, "rb"))
     feat_names = X_val.columns.tolist()
-    pdp_assign = pdp.pdp_isolate(model = model, dataset=X_val, model_features=feat_names, feature=feat)
+    pdp_assign = pdp.pdp_isolate(model = ml_model, dataset=X_val, model_features=feat_names, feature=feat)
     pdp.pdp_plot(pdp_assign, feat)
     plt.show()
+
+def shapValue(model, x_val, row_to_show=5):
+    #open ml_model
+    ml_model = pickle.load(open(model, "rb"))
+    data_for_prediction = val_X.iloc[row_to_show]
+    data_for_prediction_array = data_for_prediction.values.reshape(1, -1)
+    #when using tree model
+    explainer = shap.TreeExplainer(ml_model)
+    shap_values = explainer.shap_values(data_for_prediction)
+    shap.initjs()
+
+    return shap.force_plot(explainer.expected_value[1], shap_values[1], data_for_prediction)

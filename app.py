45086e72c19c429e4184827775c9345b6ca9cd50
Nelsonchris1 <ogbeide331@gmail.com>
2021-06-10 09:32:33 +0100

    description added

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from explain import pdplot, perm_import, perm_import_plot, shapValue
+from desc import descriptive_message_temp as desc
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
@@ -11,78 +12,17 @@ from io import StringIO
 
 html_txt = """<font color='blue'>Upload files to Explain</font>"""
 
-st.write("Explain My")
-
 option = st.sidebar.selectbox("Select view", ("Home", "ML Explain", "Tutorial"))
 if option == "Home":
-    result_temp2 ="""
-	<div style="background-color:#464e5f;padding:10px;border-radius:10px;margin:10px;">
-	<h4 style="color:white;text-align:center;">Algorithm:: {}</h4>
-	<img src="https://images.unsplash.com/photo-1545231027-637d2f6210f8?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bG9nb3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80" alt="Avatar" style="vertical-align: middle;float:left;width: 50px;height: 50px;border-radius: 50%;" >
-	<br/>
-	<br/>	
-	<p style="text-align:justify;color:white">{} % probalibilty that Patient {}s</p>
-	</div>
-	"""
-    prescriptive_message_temp ="""
-	<div style="background-color:silver;overflow-x: auto; padding:10px;border-radius:5px;margin:10px;">
-		<h3 style="text-align:justify;color:black;padding:10px">Recommended Life style modification</h3>
-		<ul>
-		<li style="text-align:justify;color:black;padding:10px">Exercise Daily</li>
-		<li style="text-align:justify;color:black;padding:10px">Get Plenty of Rest</li>
-		<li style="text-align:justify;color:black;padding:10px">Exercise Daily</li>
-		<li style="text-align:justify;color:black;padding:10px">Avoid Alchol</li>
-		<li style="text-align:justify;color:black;padding:10px">Proper diet</li>
-		<ul>
-		<h3 style="text-align:justify;color:black;padding:10px">Medical Mgmt</h3>
-		<ul>
-		<li style="text-align:justify;color:black;padding:10px">Consult your doctor</li>
-		<li style="text-align:justify;color:black;padding:10px">Take your interferons</li>
-		<li style="text-align:justify;color:black;padding:10px">Go for checkups</li>
-		<ul>
-	</div>
-	"""
-
-    descriptive_message_temp ="""
-	<div style="background-color:black;overflow-x: auto; padding:10px;border-radius:10px;margin:10px;">
-		<h3 style="text-align:justify;color:white;padding:10px">What is Hepatitis B?</h3>
-		<p style="color:white;">Hepatitis B is an infection of your liver. It’s caused by a virus. There is a vaccine that protects against it. For some people, hepatitis B is mild and lasts a short time. These “acute” cases don’t always need treatment. But it can become chronic. If that happens, it can cause scarring of the organ, liver failure, and cancer, and it even can be life-threatening.It’s spread when people come in contact with the blood, open sores, or body fluids of someone who has the hepatitis B virus.
-It's serious, but if you get the disease as an adult, it shouldn’t last a long time. Your body fights it off within a few months, and you’re immune for the rest of your life. That means you can't get it again. But if you get it at birth, it’ unlikely to go away.
-</p></div>
-<div style="background-color:black;overflow-x: auto; padding:10px;border-radius:10px;margin:10px;">
-<p><h3 style="color:white;" ><bold>Hepatitis B Symptoms</bold></h3>
-<p style="color:white;">Short-term (acute) hepatitis B infection doesn’t always cause symptoms. For instance, it’s uncommon for children younger than 5 to have symptoms if they’re infected.
-If you do have symptoms, they may include:</p>
-<li style="color:white;" >Jaundice (Your skin or the whites of the eyes turn yellow, and your pee turns brown or orange.)</li>
-<li style="color:white;" >Light-colored poop</li>
-<li style="color:white;" >Fever</li>
-<li style="color:white;" >Fatigue that persists for weeks or months</li>
-<li style="color:white;" >Stomach trouble like loss of appetite, nausea, and vomiting</li>
-<li style="color:white;" >Belly pain</li>
-<li style="color:white;" >Joint pain</li>
-<p style="color:white;">Symptoms may not show up until 1 to 6 months after you catch the virus. You might not feel anything. About a third of the people who have this disease don’t. They find out only through a blood test.
-Symptoms of long-term (chronic) hepatitis B infection don’t always show up, either. If they do, they may be like those of short-term (acute) infection.
-<br>
-<h3><bold style="color:white;">Hepatitis B Causes and Risk Factors</bold></h3>
-<p style="color:white;">It’s caused by the hepatitis B virus, and it can spread from person to person in certain ways. You can spread the hepatitis B virus even if you don’t feel sick.The most common ways to get hepatitis B include:</p>
-<li style="color:white;" >Sex</li>
-<li style="color:white;" >Sharing needles</li>
-<li style="color:white;" >Accidental needle sticks</li>
-<li style="color:white;" >Mother to child(Pregnancy)</li></p>
-<p style="color:white;">The number of people who get this disease is down, the CDC says. Rates have dropped from an average of 200,000 per year in the 1980s to around 20,000 in 2016. People between the ages of 20 and 49 are most likely to get it.
-About 90 of infants and 25-50 of children between the ages of 1-5 will become chronically infected. In adults, approximately 95% will recover completely and will not go on to have a chronic infection.</p>
-<br/>
-<br/></div>
-<div style="background-color:black;overflow-x: auto; padding:10px;border-radius:10px;margin:10px;">
-<li><a href="https://www.webmd.com/hepatitis/digestive-diseases-hepatitis-b">Learn more about Hepatitis B</a></li>    
-</div>
-	"""
-
-    st.write(result_temp2, unsafe_allow_html=True)
-    st.write(prescriptive_message_temp, unsafe_allow_html=True)
-    st.write(descriptive_message_temp, unsafe_allow_html=True)
+    st.write(desc, unsafe_allow_html=True)
 
 elif option == "ML Explain":
+    st.write("Explain My Model")
+
+    radio_option = ["None","Permutation Importance","Partial Density Plot", "Shap Values", "All"]
+    selected_explain = st.radio("Choose page:", radio_option)
+
+    
     st.sidebar.write(html_txt, unsafe_allow_html=True)
     train = st.sidebar.file_uploader("X_train", type=["csv", "text"])
     if train :
@@ -149,8 +89,7 @@ elif option == "ML Explain":
 
     # if  st.sidebar.button("Submit upload files"):
     
-    radio_option = ["None","Permutation Importance","Partial Density Plot", "Shap Values", "All"]
-    selected_explain = st.radio("Choose page:", radio_option)
+    
 
     if selected_explain == "Permutation Importance":
          #firstly compute importance and then plot perm_importance _plot
---
+++ desc.py
@@ -0,0 +1,49 @@
+#Descripion of what each explain component is 
+
+descriptive_message_temp ="""
+	<div style="background-color:black;overflow-x: auto; padding:10px;border-radius:5px;margin:10px;">
+		<h2 style="text-align:justify;color:white;padding:10px">Permutation Importance</h2>
+        <h3 style="text-align:justify;color:white;padding:10px">What it is</h3>
+		<p style="color:white;">As the name implies, Permutation feature importance describes what feature from our dataset had the most impact in building our prediction model. Permutation importance is easy to understand and also fast to process. 
+        It is measured by checking how much your scoring metric reduces when each feature is missing from the dataset, This basically means that by repeatedly eliminating each feature, the model can be retrained to determine which function is more important and which is not.</p>
+        <p style="color:white;text-align:justify">But in reality retraining, the model by eliminating each feature can be computationally expensive, to avoid removing features and retraining, the values in a feature is reshuffled which implies random noise, this is how permutation feature is computed</p>
+        <h3 style="text-align:justify;color:white;padding:10px">Questions it can answer</h3>
+        <li style="color:white;">Which features were most important</li>
+        <li style="color:white;">Which features were least important</li>
+        <li style="color:white;">What is the weight of the most important feature comapared to second and third feature</li>
+        <br></br>
+        </div>
+    <div style="background-color:black;overflow-x: auto; padding:10px;border-radius:5px;margin:10px;">
+        <h2 style="text-align:justify;color:white;padding:10px">Parital Dependancy Plot</h2>
+        <h3 style="text-align:justify;color:white;padding:10px">What it is</h3>
+        <p style="color:white;">Permutation importance indicates what feature contributed more to the model building,
+         but how can we see the impact of these individual important feature. Partial dependency plot helps to visualize individual features to see their impact on the model. Partial dependency plot helps to better
+        understand the relationship between predictors and the mode. Knowing what feature is influencing the outcome 
+        of the model is great but also  know what direction it is influencing helps for better understanding.</p>
+        <h3 style="text-align:justify;color:white;padding:10px">Questions it can answer</h3>
+        <li style="color:white;">What are the effect of the important feature on the predictor</li>
+        <li style="color:white;">What is the target distribution as well as prediction distribution</li>
+        <p></p>
+        <h3 style="text-align:justify;color:white;padding:10px">NOTE!!</h3>
+        <p style="color:white;">Higher value on the y axis means the corresponding value on the x axis has a greater influence on the predicting class</p>
+    </div>
+    <div style="background-color:black;overflow-x: auto; padding:10px;border-radius:10px;margin:10px;">
+        <h2 style="text-align:justify;color:white;padding:10px">SHAP(SHapley Additive exPlanations) values</h2>
+        <h3 style="text-align:justify;color:white;padding:10px">What it is</h3>
+        <p style="color:white;">
+            Permutation importance tells us the most important and least important features, 
+            Partial dependency tells us the effect of these features on the prediction class, 
+            Now the SHAP value breaks downs how the model works for both individual or multiple prediction(In our case,  ROW). 
+            Now we can see exactly why the outcome of a prediction is the way it is due to the help of  Shap Value.
+            <br></br>
+            Lets use an example to explain how it works.
+        We have 5 features namely Age, sex, location, salary, BuyOrNot to build a model that tells us if a client will 
+        buy our goods or not. We have build our model and what to break down on why clients are likely not to buy.
+        <br></br>
+        Using 1 row from the dataset "[Age: 20, sex: Female, location: London, BuyOrNot: NotBuy]
+        With partial dependency plot, we can see how the value age 20 impacts the prediction, 
+        what if we want to see the impact of each value from all features all at once, SHAP Value gives room for this.
+        </p>
+    </div>
+
+	"""

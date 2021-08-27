45e03447688056fa511c29e8932e9630b43c2894
Nelsonchris1 <ogbeide331@gmail.com>
2021-08-27 13:11:22 +0100

    deleted uncessary files

--- remove.py
+++ remove.py
@@ -3,7 +3,7 @@ import stat
 
 
 def remove_files():
-    files = ['model2', 'img_pdplot.png', 'shapvalue.png']
+    files = ['model2', 'img_pdplot.png', 'shapvalue.png', 'lime.html']
 
     for file in files:
         if os.path.exists(file):
